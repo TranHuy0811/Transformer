c8b682f9a4b3c7deb7098064b6978675c0adcafe
TranHuy0811 <huy08112004fo4@gmail.com>
2024-12-28 19:16:30 +0700

    add GELU function

--- feed_forward_network.py
+++ feed_forward_network.py
@@ -3,17 +3,18 @@ import torch.nn as nn
 import torch.nn.functional as F
 
 class Feed_Forward_Network(nn.Module):
-    def __init__(self, d_model, expand_factor, drop_prob):
+    def __init__(self, d_model, expand_factor, drop_prob, gelu_activation=True):
         super().__init__()
 
         self.linear1 = nn.Linear(d_model, d_model * expand_factor)
         self.dropout = nn.Dropout(p=drop_prob)
+        self.activate_func = nn.GELU() if gelu_activation else nn.ReLU()
         self.linear2 = nn.Linear(d_model * expand_factor, d_model)
 
 
     def forward(self, x):
         x = self.linear1(x)
-        x = F.relu(x)
+        x = self.activate_func(x)
         x = self.dropout(x)
         x = self.linear2(x)
         return x
